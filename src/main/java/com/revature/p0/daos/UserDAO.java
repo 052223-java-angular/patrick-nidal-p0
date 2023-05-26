0aee72ab5a10f036897151e12a8be78c6c2e0102
Patrick McLean <lavillelumiere8@gmail.com>
2023-05-25 20:32:59 -0400

    Re-added dao interface that got remvoed

--- src/main/java/com/revature/p0/daos/UserDAO.java
+++ src/main/java/com/revature/p0/daos/UserDAO.java
@@ -7,17 +7,19 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.io.IOException;
+import java.util.List;
 
-public class UserDAO implements CrudDAO {
+public class UserDAO implements CrudDAO<User> {
 
     public void save(User user) {
         try(Connection conn = ConnectionFactory.getInstance().getConnection()) {
-            String sql = "INSERT INTO accounts (id, username, password) VALUES (?, ?, ?)";
+            String sql = "INSERT INTO accounts (id, username, password, role_id) VALUES (?, ?, ?, ?)";
 
             try(PreparedStatement ps = conn.prepareStatement(sql)) {
                 ps.setString(1, user.getId());
                 ps.setString(2, user.getUsername());
                 ps.setString(3, user.getPassword());
+                ps.setString(4, user.getRoleId());
                 ps.executeUpdate();
             }
 
@@ -28,5 +30,25 @@ public class UserDAO implements CrudDAO {
         } catch(ClassNotFoundException e) {
             throw new RuntimeException("Unable to load JDBC driver", e);
         }
+    }
+
+    @Override
+    public void update(String id) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public User findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
     }
 }
