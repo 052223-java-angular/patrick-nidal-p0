e6a4d3e39bf55507c820721daa09cb1c578ded53
Nidal Alafandi <niafandi@gmail.com>
2023-05-27 00:39:48 -0400

    ProductDAO work

--- src/main/java/com/revature/p0/daos/ProductDAO.java
+++ src/main/java/com/revature/p0/daos/ProductDAO.java
@@ -41,18 +41,18 @@ public class ProductDAO implements CrudDAO {
 
 
     public List<Product> findProductByCategory(String categoryId) {
-
+        List<Product> products = new ArrayList<>();
         try(Connection conn = ConnectionFactory.getInstance().getConnection()) {
-            List<Product> products = new ArrayList<>();
             String sql = "SELECT * FROM products WHERE category_id = ?";
-
 
             try(PreparedStatement ps = conn.prepareStatement(sql)) {
                 ps.setString(1, categoryId);
 
                 ResultSet rs = ps.executeQuery();
                 if (rs.next()) {
-                    Product product = new Product(rs.getString(2), );
+                    Product product = new Product(rs.getString("description"),
+                            rs.getDouble("price"), rs.getInt("on_hand") );
+                    products.add(product);
                 }
             }
 
@@ -64,12 +64,6 @@ public class ProductDAO implements CrudDAO {
             throw new RuntimeException("Unable to load JDBC driver", e);
         }
 
-
-
-
-
-
-
-        return null;
+        return products;
     }
 }
