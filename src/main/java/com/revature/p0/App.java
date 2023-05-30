5bbdecf07ead586b302fe6606756ac7666086907
Patrick McLean <lavillelumiere8@gmail.com>
2023-05-29 23:22:57 -0400

    add a bit of logging

--- src/main/java/com/revature/p0/App.java
+++ src/main/java/com/revature/p0/App.java
@@ -5,13 +5,22 @@ import com.revature.p0.models.Session;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class App {
+
+  private static final Logger logger = LogManager.getLogger(App.class);
   public static void main(String args[]) throws SQLException, IOException, ClassNotFoundException {
+    logger.info("-------------START APPLICATION-------------");
+
     System.out.println("Welcome to eCommerce App!");
     Scanner scanner = new Scanner(System.in);
     RouterService router = new RouterService(new Session());
     router.navigate("/home", scanner);
+
+    logger.info("-------------END APPLICATION-------------");
+
     scanner.close();
   }
 }
--- src/main/java/com/revature/p0/screens/HomeScreen.java
+++ src/main/java/com/revature/p0/screens/HomeScreen.java
@@ -3,14 +3,20 @@ package com.revature.p0.screens;
 import com.revature.p0.models.Session;
 import com.revature.p0.services.RouterService;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 @AllArgsConstructor
 public class HomeScreen implements IScreen {
+    private static final Logger logger = LogManager.getLogger(HomeScreen.class);
     private final RouterService router;
 
     @Override
     public void start(Scanner scan) {
+
+        logger.info("Navigated to Home screen.");
 
         exit: {
             while(true) {
@@ -26,15 +32,18 @@ public class HomeScreen implements IScreen {
 
                 switch(input.toLowerCase()) {
                     case "1":
+                        logger.info("Navigated to Login screen.");
                         router.navigate("/login", scan);
                         break;
                     case "2":
+                        logger.info("Navigated to Register screen.");
                         router.navigate("/register", scan);
                         break;
                     case "x":
                         System.out.println("\nThank you.  Come again.");
                         break exit;
                     default:
+                        logger.warn("Invalid option!");
                         clearScreen();
                         System.out.println("Invalid option!");
                         System.out.print("Press ENTER to continue.");
